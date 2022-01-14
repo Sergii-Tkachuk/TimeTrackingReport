18a1510e9986efea951904de1184ced5d4277680
Tkachuk Serhii <sergijt733@gmail.com>
2022-01-14 22:12:15 +0200

    edited

--- TimeTrackingReport/TimeTrackingReport.cpp
+++ TimeTrackingReport/TimeTrackingReport.cpp
@@ -6,8 +6,27 @@
 
 using namespace std;
 
-struct Date
+
+class Date
 {
+private:
+	enum Months
+	{
+		January,
+		February,
+		March,
+		April,
+		May,
+		June,
+		July,
+		August,
+		September,
+		October,
+		November,
+		December
+	};
+
+public:
 	int Years;
 	int Months;
 	int Days;
@@ -19,6 +38,26 @@ struct Date
 		stream >> Years >> separator;
 		stream >> Months >> separator;
 		stream >> Days;
+	}
+
+	string getNameMonth() const
+	{
+		switch (Months)
+		{
+		case January: return "January";
+		case February: return "February";
+		case March: return "March";
+		case April: return "April";
+		case May: return "May";
+		case June: return "June";
+		case July: return "July";
+		case August: return "August";
+		case September: return "September";
+		case October: return "October";
+		case November: return "November";
+		case December: return "December";
+		default: return "NONE";
+		}
 	}
 };
 
@@ -34,24 +73,13 @@ struct DataRows
 	int LoggedHours = 0;
 };
 
-
-bool operator > (const DataRows& d1, const DataRows& d2)
-{
-	return d1.Name > d2.Name || d1.date.Years > d2.date.Years || d1.date.Months > d2.date.Months;
-}
-
-bool operator < (const DataRows& d1, const DataRows& d2)
-{
-	return d1.Name < d2.Name || d1.date.Years < d2.date.Years || d1.date.Months < d2.date.Months;
-}
-
 bool operator == (const DataRows& d1, const DataRows& d2)
 {
-	return d1.Name == d2.Name && d1.date.Years == d2.date.Years && d1.date.Months == d2.date.Months;
+	return d1.Name == d2.Name && d1.Email == d2.Email && d1.date.Years == d2.date.Years && d1.date.Months == d2.date.Months;
 }
 
 
-void addData(istream &input, vector<DataRows> &rows)
+void addData(istream &input, vector<DataRows> &rows, char separator)
 {
 	string buffString;
 
@@ -60,14 +88,14 @@ void addData(istream &input, vector<DataRows> &rows)
 		stringstream buffStream(buffString);
 		DataRows buffRows;
 
-		getline(buffStream, buffRows.Name, ';');
-		getline(buffStream, buffRows.Email, ';');
-		getline(buffStream, buffRows.Department, ';');
-		getline(buffStream, buffRows.Position, ';');
-		getline(buffStream, buffRows.Project, ';');
-		getline(buffStream, buffRows.Task, ';');
+		getline(buffStream, buffRows.Name, separator);
+		getline(buffStream, buffRows.Email, separator);
+		getline(buffStream, buffRows.Department, separator);
+		getline(buffStream, buffRows.Position, separator);
+		getline(buffStream, buffRows.Project, separator);
+		getline(buffStream, buffRows.Task, separator);
 
-		getline(buffStream, buffString, ';');
+		getline(buffStream, buffString, separator);
 		buffRows.date.getDate(buffString);
 
 		buffStream >> buffRows.LoggedHours;
@@ -79,36 +107,46 @@ void addData(istream &input, vector<DataRows> &rows)
 void PrintData(ostream& out, vector<DataRows> arrData)
 {
 	for (const auto& elem : arrData)
-		out << elem.Name << ';' << elem.date.Years << '-' << elem.date.Months << ';' << elem.LoggedHours << '\n';
+		out << elem.Name << ';' << elem.date.getNameMonth() << ' ' << elem.date.Years << ';' << elem.LoggedHours << '\n';
 }
+
 
 void Report(vector<DataRows> arr)
 {
 	for (int i = 0; i < arr.size() - 1; i++)
-	{
 		for (int j = i + 1; j < arr.size(); ++j)
-		{
 			if (arr[i] == arr[j])
 			{
 				arr[i].LoggedHours += arr[j].LoggedHours;
 				arr.erase(arr.begin() + j);
 			}
-		}
-	}
-	PrintData(cout, arr);
+	ofstream out("Time_report.txt");
+
+	out << "Name;Month;Total hours\n";
+	PrintData(out, arr);
+
+	out.close();
 }
 
 int main()
 {
-	ifstream inFile("data_file.txt");
-	
-	vector<DataRows> dataRows;
-	string columnName;
-	getline(inFile, columnName);
+	ifstream inFile("data_file.txt"); //open csv file
 
-	while(inFile)
-		addData(inFile, dataRows);
-	inFile.close();
+	if (!inFile.is_open())
+	{
+		cout << "Error: File not fount!!!\n";
+	}
+	else
+	{
+		vector<DataRows> dataRows; //
+		string columnName;
 
-	Report(dataRows);
+		getline(inFile, columnName);
+
+		addData(inFile, dataRows, ';');
+		inFile.close();
+
+		Report(dataRows);
+	}
+	return 0;
 }
