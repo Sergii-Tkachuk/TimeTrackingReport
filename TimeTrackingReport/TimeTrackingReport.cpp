0c2b816c00ea245e1615cd5c411ee7401d0945ad
Tkachuk Serhii <sergijt733@gmail.com>
2022-01-15 12:02:42 +0200

    now open the .csv file

--- TimeTrackingReport/TimeTrackingReport.cpp
+++ TimeTrackingReport/TimeTrackingReport.cpp
@@ -1,3 +1,19 @@
+/*
+Data is supplied in text file in CSV format. First line of the file is columns names. Separator is semicolon (;). It is assumed that the file is not malformed. 
+Date is in YYYY-MM-DD format.
+Input data example:
+Name;email;department;position;project;task;date;logged hours
+John Doe;john.doe@company.com;Delivery;Software Engineer;Mars;MRS-2240;2021-10-19;5
+John Doe;john.doe@company.com;Delivery;Software Engineer;Mars;MRS-2241;2021-10-20;8
+Jane Doe;jane.doe@company.com;Delivery;Manager;Venus;VNS-1111;2021-10-20;4
+......
+It is expected to receive output in CSV format, containing person name, month and total hours logged
+Output data example:
+Name;Month;Total hours
+John Doe;October 2021;13
+Jane Doe;October 2021;4
+*/
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -130,7 +146,7 @@ void Report(vector<DataRows> arr)
 
 int main()
 {
-	ifstream inFile("data_file.txt"); //open csv file
+	ifstream inFile("data_file.csv"); //open csv file
 
 	if (!inFile.is_open())
 	{
@@ -138,7 +154,7 @@ int main()
 	}
 	else
 	{
-		vector<DataRows> dataRows; //
+		vector<DataRows> dataRows;
 		string columnName;
 
 		getline(inFile, columnName);
